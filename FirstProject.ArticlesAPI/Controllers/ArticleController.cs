73226c00d21307db1bcfc99a24a0e5456f07106e
doomkinn-geek <doomkinn@yandex.ru>
2023-02-04 12:42:47 +0300

    IRepository

---
+++ FirstProject.ArticlesAPI/Controllers/ArticleController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.ArticlesAPI.Controllers
+{
+    [Route("api/articles")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        // GET: ArticleController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ArticleController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ArticleController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ArticleController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ArticleController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ArticleController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ArticleController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ArticleController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FirstProject.ArticlesAPI/Data/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace FirstProject.ArticlesAPI.Data.Interfaces
+{
+    public interface IRepository<TEntity>
+    {                
+        void Create(TEntity data);
+        void Update(TEntity data);
+        void Delete(TEntity data);
+        void Edit(TEntity data);
+        IQueryable<TEntity> FindAll();
+        Task<List<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+    }
+}
