8244043e5415a8c482e2a10753cd47075c28355b
root <root@DESKTOP-T51K5CR>
2023-02-15 11:19:24 +0300

    add html filtration for preview article DTO

--- FirstProject.ArticlesAPI/MappingProfile.cs
+++ FirstProject.ArticlesAPI/MappingProfile.cs
@@ -2,6 +2,7 @@
 using FirstProject.ArticlesAPI.Models;
 using FirstProject.ArticlesAPI.Models.DTO;
 using FirstProject.ArticlesAPI.Models.Requests;
+using FirstProject.ArticlesAPI.Utility;
 
 namespace FirstProject.ArticlesAPI
 {
@@ -13,14 +14,14 @@ namespace FirstProject.ArticlesAPI
                 .ForMember(a => a.TimePublished, opt => opt.MapFrom(source => DateTime.Parse(source.TimePublished.ToString())))
                 .ForMember(a => a.AuthorNickName, opt => opt.MapFrom(a => a.Author.NickName))
                 .ForMember(a => a.FullTextHtml, opt => opt.MapFrom(a => a.TextHtml))
-                .ForMember(a => a.PreviewTextHtml, opt => opt.MapFrom(a => a.LeadData.TextHtml))
+                .ForMember(a => a.PreviewTextHtml, opt => opt.MapFrom(a => HtmlSanitizeExtension.FilterHtmlToWhitelist(a.LeadData.TextHtml)))
                 .ForMember(a => a.ReadingCount, opt => opt.MapFrom(a => a.Statistics.ReadingCount))
                 .ForMember(a => a.HubrId, opt => opt.MapFrom(a => a.hubrId))
                 .ReverseMap();
             CreateMap<Article, PreviewArticleDTO>()
                 .ForMember(a => a.TimePublished, opt => opt.MapFrom(source => DateTime.Parse(source.TimePublished.ToString())))
                 .ForMember(a => a.AuthorNickName, opt => opt.MapFrom(a => a.Author.NickName))
-                .ForMember(a => a.Text, opt => opt.MapFrom(a => a.LeadData.TextHtml))
+                .ForMember(a => a.Text, opt => opt.MapFrom(a => HtmlSanitizeExtension.FilterHtmlToWhitelist(a.LeadData.TextHtml)))
                 .ForMember(a => a.ImageURL, opt => opt.MapFrom(a => a.LeadData.ImageUrl))
                 .ForMember(a => a.ReadingCount, opt => opt.MapFrom(a => a.Statistics.ReadingCount))
                 .ForMember(a => a.HubrId, opt => opt.MapFrom(a => a.hubrId));
