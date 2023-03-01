74618f279c9ccf39a656dd09fdfbc98078a45893
prog-5 <a59-info@yandex.ru>
2023-03-01 10:26:19 +0300

    last main&create_article

---
+++ FirstProject.ArticlesAPI/Utility/HtmlImgFilter.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace FirstProject.ArticlesAPI.Utility
+{    
+    public static class HtmlImgFilter
+    {        
+        /// <summary>
+        /// Метод, который удаляет тэг img из входящего html
+        /// </summary>
+        public static string FilterHtmlToNoImg(string text)
+        {            
+            Regex regex = new Regex(@"\<img[^\>]+\>", System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            return regex.Replace(text, "");
+        }
+    }
+}
