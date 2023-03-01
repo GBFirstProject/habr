81907c9ce82e231e46a049ce04db02ef3776e45b
Raixther <g.a.z@bk.ru>
2023-03-01 17:19:08 +0300

    small fixes

---
+++ FirstProject.Web/Pages/CreateArticle.cshtml.cs
@@ -0,0 +1,46 @@
+using Duende.Bff;
+
+using FirstProject.ArticlesAPI.Models.Requests;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace FirstProject.Web.Pages
+{
+
+   [BffApi]
+   public class CreateArticleModel : PageModel
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        private readonly string _articleAPIUrl;
+
+        public CreateArticleModel(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+           _httpClientFactory= httpClientFactory;
+            _articleAPIUrl = configuration.GetSection("ServicesUrl")["ArticlesAPI"];
+        }       
+       public void OnGet()
+       {
+       
+       } 
+       public async Task OnPostAsync([FromForm] string title, bool commentsEnabled, string textHtml,
+       List<string> tags, List<string> hubs) 
+       { 
+          var client = _httpClientFactory.CreateClient();
+           var content = JsonContent.Create(new CreateArticleRequest()
+           {
+           Title = title,
+           CommentsEnabled = commentsEnabled,
+           TextHtml = textHtml,  
+           Tags= tags,
+           Hubs = hubs,     
+            });
+            var request = new HttpRequestMessage(HttpMethod.Post, _articleAPIUrl + "/articles" +"/add-article");
+           request.Content = content;
+           await client.SendAsync(request);
+         
+       }
+    }
+}
