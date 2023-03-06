1aea6bf9baf0983ace930ad93caacfeeb53bf00e
dmitriynt <dmitriynt@gmail.com>
2023-03-06 22:59:31 +0500

    Added UserName and Blocked

--- FirstProject.AuthAPI/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ FirstProject.AuthAPI/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,6 +47,10 @@ namespace FirstProject.AuthAPI.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [Display(Name = "Login")]
+            public string UserName { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -75,7 +79,7 @@ namespace FirstProject.AuthAPI.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.UserName, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
