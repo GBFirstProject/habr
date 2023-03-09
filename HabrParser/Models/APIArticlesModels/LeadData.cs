f8d3af4c17a2d741e254d4be4ebb40f0e076d8b5
root <root@DESKTOP-T51K5CR>
2023-03-09 20:36:39 +0300

    fix parser

--- HabrParser/Models/APIArticlesModels/LeadData.cs
+++ HabrParser/Models/APIArticlesModels/LeadData.cs
@@ -7,6 +7,8 @@ namespace HabrParser.Models.APIArticles
     [Table("LeadData")]
     public class LeadData : BaseModel<Guid>
     {
+        public Guid ArticleId { get; set; }
+        [ForeignKey("ArticleId")]
         public Article Article { get; set; }
         public string TextHtml { get; set; } = string.Empty;        
         public string? ImageUrl { get; set; } = string.Empty;
